69b4402154576f6907393740f16769c847741a24
Zayed-Rahat <azurahat129@gmail.com>
2022-07-05 23:31:54 +0600

    categoryList

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,11 @@ import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
 import NewArrivals from "../components/home/NewArrivals";
 import BestSellers from "../components/home/BestSellers";
+import CategoryList from "../components/category/CategoryList";
 
 const Home = () => {
   return (
     <>
-    <br/>
-    <br/>
       <div className="jumbotron text-danger h1 font-weight-bold text-center">
         <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
       </div>
@@ -21,6 +20,11 @@ const Home = () => {
         Best Sellers
       </h4>
       <BestSellers />
+
+      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+        Categories
+      </h4>
+      <CategoryList />
 
       <br />
       <br />
